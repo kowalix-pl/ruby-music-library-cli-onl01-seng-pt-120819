d5da09eea754de219b0087ac5aa0aa60ca4d64f5
kowalix007 <kowalix007@gmail.com>
2020-02-09 18:56:56 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -93,8 +93,8 @@ end
 #     if (1..Song.all.length).include?(input)
 #     song = Song.all.sort_by(:&:name)[input - 1]
 #   end 
-  puts "Playing #{song.name} by #{song.artist.name}"
-end
+#   puts "Playing #{song.name} by #{song.artist.name}"
+# end
   
   end 
 end
